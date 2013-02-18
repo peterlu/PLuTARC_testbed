ef3e1c4c91b3cc936cf6e4e605d3882cb3b7e1c5
Peter J. Lu <plu@fas.harvard.edu>
2013-02-18 07:03:47 -0500

    initial commit

---
+++ phi41pct_130210.sh
@@ -0,0 +1,6 @@
+./plu_centerfind2d phi41pct_3D_6zoom phi41pct_3D_6zoom 1 141 1 3 6 4 3 3 5 0
+./plu_link3dt phi41pct_3D_6zoom 5 3 5 0.1 .0803 0.2938 0.5 3
+./plu_struct3dt phi41pct_3D_6zoom 1 3 1 0.51 1.2 1.8 5 100
+./hoki_3dcheck phi41pct_3D_6zoom 1 0.0803 0.2938 0.5
+gnuplot gnuplot_g_of_r_comparison_130210.txt 
+
